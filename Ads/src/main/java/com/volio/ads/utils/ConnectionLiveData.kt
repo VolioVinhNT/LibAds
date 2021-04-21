10ae615177b7e1f969e8be94be2f248a436d3363
Nam Đặng <dangnam197@gmail.com>
2021-04-21 17:05:58 +0700

    fix crash ConnectionLiveData

--- Ads/src/main/java/com/volio/ads/utils/ConnectionLiveData.kt
+++ Ads/src/main/java/com/volio/ads/utils/ConnectionLiveData.kt
@@ -7,22 +7,16 @@ import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
 import android.os.Build
-import android.util.Log
 import androidx.lifecycle.LiveData
-
 
 class ConnectionLiveData
 constructor(var context: Context) : LiveData<Boolean>() {
-
     private var connectivityManager: ConnectivityManager =
         context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
-
     private lateinit var connectivityManagerCallback: ConnectivityManager.NetworkCallback
-
     override fun onActive() {
         super.onActive()
         postValue(false)
-
         val networkCallback: ConnectivityManager.NetworkCallback =
             object : ConnectivityManager.NetworkCallback() {
                 override fun onAvailable(network: Network) {
@@ -35,22 +29,20 @@ constructor(var context: Context) : LiveData<Boolean>() {
                     postValue(false)
                 }
             }
-
         val connectivityManager =
             context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
-
         //unregister first
         try {
             connectivityManager.unregisterNetworkCallback(networkCallback)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                connectivityManager.registerDefaultNetworkCallback(networkCallback)
+            } else {
+                val request = NetworkRequest.Builder()
+                    .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET).build()
+                connectivityManager.registerNetworkCallback(request, networkCallback)
+            }
         } catch (e: Exception) {
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            connectivityManager.registerDefaultNetworkCallback(networkCallback)
-        } else {
-            val request = NetworkRequest.Builder()
-                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET).build()
-            connectivityManager.registerNetworkCallback(request, networkCallback)
+            postValue(true)
         }
     }
 
