443df346bdd51ebae93475d911136b383820c4c8
vinhnt2395 <vinhnt@volio.vn>
2024-01-11 14:50:33 +0700

    add module cmp

---
+++ Ads/src/main/java/com/volio/cmp/CMPCallback.kt
@@ -0,0 +1,7 @@
+package com.volio.cmp
+
+interface CMPCallback {
+    fun onShowAd()
+
+    fun onChangeScreen()
+}
---
+++ Ads/src/main/java/com/volio/cmp/CMPController.kt
@@ -0,0 +1,96 @@
+package com.volio.cmp
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import com.google.android.ump.ConsentDebugSettings
+import com.google.android.ump.ConsentRequestParameters
+import com.google.android.ump.UserMessagingPlatform
+import java.util.ArrayList
+
+class CMPController  constructor(private var activity: Activity) {
+
+    fun isGDPR(): Boolean {
+        return GDPRUtils.isGDPR(activity)
+    }
+
+
+    fun showCMP() {
+        UserMessagingPlatform.loadConsentForm(activity, {
+//            dialog.dismissDialog()
+            it.show(activity) {
+                val canRequestAds = GDPRUtils.canShowAds(activity)
+//                if (canRequestAds) {
+//                    AdsUtils.isNotConsent = false
+//                } else {
+//                    AdsUtils.isNotConsent = true
+//                }
+            }
+        }, {
+//            dialog.dismissDialog()
+        })
+    }
+
+    fun showCMP(isTesting: Boolean, cmpCallback: CMPCallback) {
+        val debugSettings = ConsentDebugSettings.Builder(activity)
+            .setDebugGeography(ConsentDebugSettings.DebugGeography.DEBUG_GEOGRAPHY_EEA)
+            .addTestDeviceHashedId(Utils.getDeviceID(activity)).build()
+
+        val params = ConsentRequestParameters.Builder().setTagForUnderAgeOfConsent(false)
+            .setConsentDebugSettings(if (isTesting) debugSettings else null).build()
+
+        val consentInformation = UserMessagingPlatform.getConsentInformation(activity)
+        consentInformation.requestConsentInfoUpdate(activity, params, {
+            val canRequestAds = GDPRUtils.canShowAds(activity)
+           val canrequest =  consentInformation.canRequestAds()
+            if (canRequestAds) {
+                GDPRUtils.isUserConsent = true
+                cmpCallback.onShowAd()
+            } else {
+                UserMessagingPlatform.loadConsentForm(activity, {
+                    it.show(activity) {
+                        val canRequestAds = GDPRUtils.canShowAds(activity)
+                        if (canRequestAds) {
+                            GDPRUtils.isUserConsent = true
+                            cmpCallback.onShowAd()
+//                            showAd()
+                        } else {
+                            GDPRUtils.isUserConsent = false
+                            cmpCallback.onChangeScreen()
+//                            changeScreen()
+                        }
+                    }
+                }, {
+                    cmpCallback.onShowAd()
+                })
+            }
+        }, { requestConsentError ->
+            cmpCallback.onChangeScreen()
+        })
+    }
+
+
+    companion object {
+
+//        @SuppressLint("StaticFieldLeak")
+//        private lateinit var cmpController: CMPController
+//
+//
+//        fun init(
+//            activity: Activity,
+//        ) {
+//            cmpController = CMPController(activity)
+//        }
+//
+//
+//        fun getInstance(): CMPController {
+//            if (!::cmpController.isInitialized) {
+//                throw Throwable("call init")
+//            }
+//            return cmpController
+//        }
+//
+//        fun checkInit() = ::cmpController.isInitialized
+    }
+}
