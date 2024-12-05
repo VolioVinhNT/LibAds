ff36803d30a3b8e51fd29b31c9436bafc01c3651
VolioVinhNT <vinhnt@volio.vn>
2024-12-05 10:43:27 +0700

    fix bug log ad revenue colorlight

--- Ads/src/main/java/com/volio/ads/AdsController.kt
+++ Ads/src/main/java/com/volio/ads/AdsController.kt
@@ -221,6 +221,7 @@ class AdsController private constructor(
     }
 
     public fun initAppFlyer(application: Application) {
+        isUseAppflyer = true
         val afRevenueBuilder = AppsFlyerAdRevenue.Builder(application)
         AppsFlyerAdRevenue.initialize(afRevenueBuilder.build())
         AppsFlyerLib.getInstance()
